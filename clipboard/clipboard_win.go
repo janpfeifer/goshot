29ab384017a1da8ce8c7416e3c92ce0db467dadb
Jan Pfeifer <pfeifer@gmail.com>
2021-06-12 08:00:15 +0200

    Fixing windows clipboard: works with CF_DIBV5 ... but Chrome doesn't seem to support it :((

--- clipboard/clipboard_win.go
+++ clipboard/clipboard_win.go
@@ -28,29 +28,27 @@ const (
 
 func CopyImage(img image.Image) error {
 	glog.V(2).Infof("CopyImage(bounds=%+v)", img.Bounds())
-	hBitmap, err := hBitmapFromImage(img)
+	hBitmap, bitmapHeader, bitmapBits, err := hBitmapFromImage(img)
 	if err != nil {
 		return err
 	}
-	writeHBitmap(hBitmap)
-	return nil
-}
 
-// waitOpenClipboard opens the clipboard, polling every 3 milliseconds
-// and waiting for up to a few seconds to do so (`clipboardOpenMaxTime`).
-func waitOpenClipboard() error {
-	started := time.Now()
-	limit := started.Add(clipboardOpenMaxTime)
-	for time.Now().Before(limit) {
-		if win.OpenClipboard(0) {
-			return nil
-		}
-		time.Sleep(3 * time.Millisecond)
+	// CF_DIBV5 version
+	data, err := bitmapToGlobalAlloc(bitmapHeader, bitmapBits)
+	if err != nil {
+		return err
 	}
-	return errors.New("failed win.OpenClipboard()")
+	win.DeleteObject(win.HGDIOBJ(hBitmap)) // hBitmap is no longer needed, once copied.
+
+	return safeSetClipboardData(win.CF_DIBV5, win.HANDLE(data))
 }
 
-func writeHBitmap(hBitmap win.HBITMAP) error {
+// safeSetClipboardData is a wrapper around all the clipboard "bureaucracy".
+// See formats and their expected contents in
+// https://docs.microsoft.com/en-us/windows/win32/dataxchg/standard-clipboard-formats.
+//
+// No support for registered formats yet -- should be simple to add.
+func safeSetClipboardData(format uint32, handle win.HANDLE) error {
 	glog.V(2).Infof("writeHBitmap()")
 
 	// LockOSThread ensure that the whole method will keep executing on the same thread from begin to end (it actually locks the goroutine thread attribution).
@@ -69,11 +67,36 @@ func writeHBitmap(hBitmap win.HBITMAP) error {
 		return errors.New("failed win.EmptyClipboard()")
 	}
 	glog.V(2).Infof("writeHBitmap: clipboard emptied.")
-	win.SetClipboardData(win.CF_BITMAP, win.HANDLE(hBitmap))
+	win.SetClipboardData(format, handle)
 	return nil
 }
 
-func hBitmapFromImage(im image.Image) (win.HBITMAP, error) {
+// Converts bitmap to a contiguous GMEM_MOVEABLE global alloc to be used the clipboard.
+// It should work with BITMAPINFOHEADER, BITMAPV4HEADER and BITMAPV5HEADER
+// if the biSize field is correctly set.
+//
+// Palette color map not supported.
+func bitmapToGlobalAlloc(bitmapHeader *win.BITMAPINFOHEADER, bitmapBits unsafe.Pointer) (win.HGLOBAL, error) {
+	glog.V(2).Infof("bitmapToGlobalAlloc: header size=%d, image size=%d", bitmapHeader.BiSize, bitmapHeader.BiSizeImage)
+	// "If the hMem parameter identifies a memory object, the object must have
+	// been allocated using the function with the GMEM_MOVEABLE flag."
+	movableDataHandle := win.GlobalAlloc(win.GMEM_MOVEABLE, uintptr(bitmapHeader.BiSize+bitmapHeader.BiSizeImage))
+	if movableDataHandle == 0 {
+		return 0, errors.New("call to GlobalAlloc failed")
+	}
+
+	lockedData := win.GlobalLock(movableDataHandle)
+	if lockedData == nil {
+		return 0, errors.New("call to GlobalLock failed")
+	}
+	win.MoveMemory(lockedData, unsafe.Pointer(bitmapHeader), uintptr(bitmapHeader.BiSize))
+	win.MoveMemory(unsafe.Pointer(uintptr(lockedData)+uintptr(bitmapHeader.BiSize)), bitmapBits, uintptr(bitmapHeader.BiSizeImage))
+	win.GlobalUnlock(movableDataHandle)
+
+	return movableDataHandle, nil
+}
+
+func hBitmapFromImage(im image.Image) (hBitmap win.HBITMAP, bitmapHeader *win.BITMAPINFOHEADER, bitmapBits unsafe.Pointer, err error) {
 	bi := &win.BITMAPV5HEADER{
 		BITMAPV4HEADER: win.BITMAPV4HEADER{
 			BITMAPINFOHEADER: win.BITMAPINFOHEADER{
@@ -102,17 +125,17 @@ func hBitmapFromImage(im image.Image) (win.HBITMAP, error) {
 	hdc := win.GetDC(0)
 	defer win.ReleaseDC(0, hdc)
 
-	var lpBits unsafe.Pointer
-
 	// Create the DIB section with an alpha channel.
-	hBitmap := win.CreateDIBSection(hdc, &bi.BITMAPINFOHEADER, win.DIB_RGB_COLORS, &lpBits, 0, 0)
+	hBitmap = win.CreateDIBSection(hdc, &bi.BITMAPINFOHEADER, win.DIB_RGB_COLORS, &bitmapBits, 0, 0)
 	switch hBitmap {
 	case 0, win.ERROR_INVALID_PARAMETER:
-		return 0, errors.New("CreateDIBSection failed")
+		return 0, nil, nil, errors.New("CreateDIBSection failed")
 	}
-	glog.V(2).Infof("header=%+v", &bi)
+	glog.V(2).Infof("header=%+v", bi)
+	glog.V(2).Infof("bitmapBits=%v", bitmapBits)
+
 	// Fill the image
-	bitmapArray := (*[1 << 30]byte)(unsafe.Pointer(lpBits))
+	bitmapArray := (*[1 << 30]byte)(unsafe.Pointer(bitmapBits))
 	i := 0
 	for y := im.Bounds().Min.Y; y != im.Bounds().Max.Y; y++ {
 		for x := im.Bounds().Min.X; x != im.Bounds().Max.X; x++ {
@@ -125,5 +148,19 @@ func hBitmapFromImage(im image.Image) (win.HBITMAP, error) {
 		}
 	}
 
-	return hBitmap, nil
+	return hBitmap, &bi.BITMAPINFOHEADER, bitmapBits, nil
+}
+
+// waitOpenClipboard opens the clipboard, polling every 3 milliseconds
+// and waiting for up to a few seconds to do so (`clipboardOpenMaxTime`).
+func waitOpenClipboard() error {
+	started := time.Now()
+	limit := started.Add(clipboardOpenMaxTime)
+	for time.Now().Before(limit) {
+		if win.OpenClipboard(0) {
+			return nil
+		}
+		time.Sleep(3 * time.Millisecond)
+	}
+	return errors.New("failed win.OpenClipboard()")
 }
